0486c3ba670436b05a52107504230a4721af3f10
KelwinJonas <kelwinjonas@gmail.com>
2021-07-03 15:36:28 -0300

    Corrigindo imagem ganhamo

--- resources/views/resultado.blade.php
+++ resources/views/resultado.blade.php
@@ -10,12 +10,12 @@
           <div class="card-body">
             <div id="div-image-result" class="row justify-content-center">
                 <div class="col-sm-8">
-                  <img src="@if($img == 1){{asset('img/ganhamo.png')}}@else{{asset('img/perdemo.jpeg')}}@endif" alt="..." style="width: 250px;">
+                  <img src="@if($img == 1){{asset('img/ganhamo.PNG')}}@else{{asset('img/perdemo.jpeg')}}@endif" alt="..." style="width: 250px;">
                 </div>
             </div>
             <h5 class="card-title">{{$msg_1}}</h5>
             <p class="card-text">{{$msg_2}}</p>
-            
+
             <a href="{{route('index')}}" class="btn btn-secondary">Início</a>
             <a id="btn-new-game" href="{{route('jogo.new')}}" class="btn btn-primary">Novo jogo</a>
           </div>
